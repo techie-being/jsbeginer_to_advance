02316a98a8fa34dd9c29677160489e8c65b9bda4
Rakesh <014rakeshofficial@gmail.com>
2025-06-27 17:47:32 +0000

    date and time

--- basics/day4.js
+++ basics/day4.js
@@ -29,3 +29,54 @@ console.log(Math.floor(Math.random() * 10) + 1)// 1 it returns a random integer 
 const min = 10
 const max = 20
 console.log(Math.floor(Math.random() * (max - min + 1)) + min) // 10 it returns a random integer between 10 and 20 wherw min is inclusive and max is inclusive
+
+
+
+/****************** date AND TIME **************** */
+// date and time important methods
+let date = new Date()
+console.log(date)  //2025-06-27T16:09:05.035Z
+console.log(date.toDateString()) //Fri Jun 27 2025
+console.log(date.toLocaleString())  //6/27/2025, 4:09:05 PM
+console.log(date.toTimeString())  //16:09:05 GMT+0000 (Coordinated Universal Time)
+console.log(date.toISOString())  //2025-06-27T16:09:05.035Z
+console.log(date.toString())   //Fri Jun 27 2025 16:09:05 GMT+0000 (Coordinated Universal Time)
+console.log(date.toJSON()) //2025-06-27T16:09:05.035Z  
+// example
+let date1 = new Date(2025, 5, 27 )
+console.log(date1)
+console.log(date1.toDateString())   //Fri Jun 27 2025
+
+let date2 = new Date("2025-06-27")   //yyyy-mm-dd format date
+console.log(date2.toLocaleString())  //6/27/2025, 12:00:00 AM
+
+let date3 = new Date("06-27-2025")   //mm-dd-yyyy format date
+console.log(date3.toLocaleString())  //6/27/2025, 12:00:00 AM
+
+
+
+
+/****************************time stamp */
+let mystamp = Date.now() // returns the number of milliseconds since January 1, 1970
+console.log(mystamp) // 1719510545035 it returns the number of milliseconds since January 1, 1970
+
+console.log(date3.getTime()) // 1719446400000 it returns the number of milliseconds since January 1, 1970 to date3
+
+// time satmp is used to compare two dates
+// comparison in seconds
+console.log(Math.floor(mystamp / 1000)) // 1719510545 it returns the number of seconds since January 1, 1970
+
+console.log(Math.floor(date3.getTime() / 1000)) // 1719446400  it returns the number of seconds since January 1, 1970 to date3 
+
+// comparison in minutes
+console.log(Math.floor(mystamp / (1000 * 60))) // 28658509 it returns the number of minutes since January 1, 1970
+console.log(Math.floor(date3.getTime() / (1000 * 60))) // 28657440 it returns the number of minutes since January 1, 1970 to date3
+
+let newdate = new Date()
+console.log(newdate.getMonth() + 1) //returns month of today date
+console.log(newdate.getDay())       // return current day no of present week
+
+// localestring methods used to customize date values
+newdate.toLocaleString('default',{
+    weekday:"long"
+})
