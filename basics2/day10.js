707b8106aa8af9b5b2235f4a72387f3ffb6296a7
Rakesh <014rakeshofficial@gmail.com>
2025-07-04 08:46:35 +0000

    control flow1

---
+++ basics2/day10.js
@@ -0,0 +1,59 @@
+/************control flow/logic floe************* */
+
+/* if else statement
+syntax
+if("condition"){
+
+}
+else{
+
+}
+
+*/
+
+/* else if syntax
+
+if first if is not executed then else if will be executed if it also fails then else will be executed
+
+if("condition1"){
+
+}
+else if{
+
+}
+else{
+
+}
+
+*/
+
+/*  wrong practice
+
+let age = 20;
+if (age >= 18) console.log("You can vote"), elseconsole.log("You can drive");
+
+*/
+
+
+/* operators
+
+// comparison operators
+== equal to
+=== equal to and data type
+!= not equal to
+!== not equal to and type
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+*/
+
+/* 
+logical operators
+
+&& and
+|| or
+! not            
+*/
+
